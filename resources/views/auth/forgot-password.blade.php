9386ed520c47db765221dfece17699a49da49927
Agofy-dev <angelgabrielocquefigueroa123@gmail.com>
2026-05-21 00:32:32 -0400

    ui: forzar mensaje de validacion nativa en espanol para el campo de correo

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -10,7 +10,7 @@
 
         <div>
             <x-input-label for="email" :value="__('Correo electrónico')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus oninvalid="this.setCustomValidity('Por favor, rellene este campo.')" oninput="this.setCustomValidity('')" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
