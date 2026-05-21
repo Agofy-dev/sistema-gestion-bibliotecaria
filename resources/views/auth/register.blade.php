7396d2d78e37756e69768a4117aac3a6c1abc5b7
Agofy-dev <angelgabrielocquefigueroa123@gmail.com>
2026-05-21 00:38:58 -0400

    ui: forzar mensajes de validacion nativa en espanol para el formulario de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,37 +3,37 @@
         @csrf
 <div>
     <x-input-label for="name" :value="__('Nombre y Apellido')" />
-    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus placeholder="Nombre y Apellido" />
+    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" oninvalid="this.setCustomValidity('Por favor, rellene este campo.')" oninput="this.setCustomValidity('')" />
     <x-input-error :messages="$errors->get('name')" class="mt-2" />
 </div>
 
 <div class="mt-4">
     <x-input-label for="cedula" :value="__('Cédula')" />
-    <x-text-input id="cedula" class="block mt-1 w-full" type="text" name="cedula" :value="old('cedula')" required placeholder="Número de Cédula" />
+    <x-text-input id="cedula" class="block mt-1 w-full" type="text" name="cedula" :value="old('cedula')" required oninvalid="this.setCustomValidity('Por favor, rellene este campo.')" oninput="this.setCustomValidity('')" />
     <x-input-error :messages="$errors->get('cedula')" class="mt-2" />
 </div>
 
 <div class="mt-4">
     <x-input-label for="telefono" :value="__('Teléfono')" />
-    <x-text-input id="telefono" class="block mt-1 w-full" type="text" name="telefono" :value="old('telefono')" placeholder="Número de Telefono" />
+    <x-text-input id="telefono" class="block mt-1 w-full" type="text" name="telefono" :value="old('telefono')" required oninvalid="this.setCustomValidity('Por favor, rellene este campo.')" oninput="this.setCustomValidity('')" />
     <x-input-error :messages="$errors->get('telefono')" class="mt-2" />
 </div>
 
 <div class="mt-4">
     <x-input-label for="email" :value="__('Correo Electrónico')" />
-    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required placeholder="Correo Electronico" />
+    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" oninvalid="this.setCustomValidity('Por favor, rellene este campo.')" oninput="this.setCustomValidity('')" />
     <x-input-error :messages="$errors->get('email')" class="mt-2" />
 </div>
 
 <div class="mt-4">
     <x-input-label for="password" :value="__('Contraseña')" />
-    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required placeholder="Ingresar Contraseña" />
+    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" oninvalid="this.setCustomValidity('Por favor, rellene este campo.')" oninput="this.setCustomValidity('')" />
     <x-input-error :messages="$errors->get('password')" class="mt-2" />
 </div>
 
 <div class="mt-4">
     <x-input-label for="password_confirmation" :value="__('Confirmar Contraseña')" />
-    <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required placeholder="Confirmar Contraseña" />
+    <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" oninvalid="this.setCustomValidity('Por favor, rellene este campo.')" oninput="this.setCustomValidity('')" />
     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
 </div>
 
